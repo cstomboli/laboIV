cf2467fdc9ed5975531b8220b6eee9f5f5402cf0
Carolina Stomboli <carostomboli@hotmail.com>
2020-08-30 21:43:42 -0300

    subo

--- my-dream-app/src/app/app.component.ts
+++ my-dream-app/src/app/app.component.ts
@@ -7,4 +7,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my-dream-app';
+
+  public ejemplo = 'Hola Mundo';
+  public edadUno = 'Edad Uno';
+  public edadDos = 'Edad Dos';
+    public ejemplo2 = 'Hola ejemplo2 Mundo';
+    constructor() { }
+
+    ngOnInit() {
+    }
+
+    PruebaClick() {
+      this.ejemplo2 = 'Ejecutamos el evento!!!';
+    }
 }
